34a3351bde94020f4075f0f020f9a526029d48cb
RakVish1342 <rakshith.vishwanatha@gmail.com>
2020-04-16 02:34:56 -0700

    Moved scripts backup folder to scripts_bak folder in root directory since those execs were being read instead of execs in scripts/. Also deleted devel/ folder from being tracked.

--- catkin_ws_py/devel/share/simple_arm/cmake/simple_arm-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(simple_arm_MESSAGE_FILES "")
-set(simple_arm_SERVICE_FILES "/home/robond/Documents/rakshith/ASU/sem2/eee_587_optimal_ctrl/project/catkin_ws_py/src/simple_arm/srv/GoToPosition.srv")
